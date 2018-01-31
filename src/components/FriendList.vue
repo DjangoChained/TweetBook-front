36e7597b59bd1c096456b214162e39aca3fb0c7c
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-01-31 16:00:03 +0100

    Page de liste des amis

---
+++ src/components/FriendList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+      <h2>Trouver de nouveaux amis</h2>
+      <form class="form-inline mb-5">
+        <div class="input-group w-100">
+          <input type="text" class="form-control" placeholder="Entrez un nom" />
+          <div class="input-group-append">
+            <button class="btn btn-secondary" type="button">Rechercher</button>
+          </div>
+        </div>
+      </form>
+      <h2>Mes amis</h2>
+      <friendbutton name="Ami 1"></friendbutton>
+      <friendbutton name="Ami 2"></friendbutton>
+      <friendbutton name="Ami 3"></friendbutton>
+  </div>
+</template>
+
+<script>
+import friendbutton from './friendbutton.vue'
+export default {
+  name: 'FriendList',
+  components: {
+    friendbutton
+  }
+}
+</script>
