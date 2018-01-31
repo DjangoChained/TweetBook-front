3506a193e60637b2d0d03bce6a43ef408175a23a
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-01-31 16:00:23 +0100

    Verrouillage des pages réservées aux utilisateurs authentifiés

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import FriendList from '@/components/FriendList'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/friends/list',
+      name: 'FriendList',
+      component: FriendList,
+      secure: true
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  router.options.routes.forEach((route) => {
+    if (to.matched[0].path === route.path && route.secure && !router.app.logged_on) {
+      return next('/')
+    }
+  })
+  next()
+})
+
+export default router
