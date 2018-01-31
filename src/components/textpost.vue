1dc99cc83b71ea690a8b532403b96d5959db9131
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-01-31 11:37:34 +0100

    Composant de publication texte

---
+++ src/components/textpost.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="post">
+        <div class="post-content">
+            <div class="content">
+                <p class="name">{{ author }}</p>
+                <p>{{ content }}</p>
+            </div>
+            <div class="btn-group-vertical">
+                <button type="button" class="btn btn-success" v-bind:class="{ active: reaction == 'like' }" v-on:click="reaction = (reaction == 'like' ? '' : 'like')"><i class="fa fa-thumbs-up"></i></button>
+                <button type="button" class="btn btn-danger" v-bind:class="{active: reaction == 'dislike' }" v-on:click="reaction = (reaction == 'dislike' ? '' : 'dislike')"><i class="fa fa-thumbs-down"></i></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'textpost',
+  props: ['author', 'content'],
+  data: function () {
+    return {
+      reaction: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+    margin-bottom: 0.5em;
+}
+p.name {
+    color: #446E9B;
+    font-weight: bold;
+    font-size: 90%;
+}
+.post {
+    border-radius: 5px;
+    border: 1px solid lightgray;
+    margin-bottom: 2em;
+}
+.post-content {
+    position: relative;
+    width: 100%;
+    padding: 1em;
+    padding-right: 4em;
+    min-height: 6em;
+}
+.btn-group-vertical {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 3em;
+}
+.btn-group-vertical button {
+    min-height: 3em;
+    height: 50%;
+}
+</style>
+
