db2e580a71d5d3a70334e27af4dbcbbfbe1a6155
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-01-31 11:37:15 +0100

    Page d'index

---
+++ src/components/Index.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 col-md-6">
+      <div class="jumbotron">
+        <h2>La crème<br />{{ joke }}<br />à portée de main.</h2>
+        <p class="lead">TweetBook n'accepte que le meilleur.</p>
+      </div>
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <div class="text-center" v-if="currentForm == 0">
+        <h3>Rejoignez-nous</h3>
+        <form>
+          <fieldset>
+            <input type="text" class="form-control" placeholder="Nom de famille" autocomplete="family-name" v-model="lastName" required />
+            <input type="text" class="form-control" placeholder="Prénom" autocomplete="given-name" v-model="firstName" required />
+            <input type="date" class="form-control" placeholder="Date de naissance" autocomplete="bday" v-model="birthDate" required />
+            <input type="email" class="form-control" placeholder="Adresse e-mail" v-model="email" required />
+            <input type="text" class="form-control" placeholder="Nom d'utilisateur" autocomplete="username" v-model="newUsername" required />
+            <input type="password" class="form-control" placeholder="Mot de passe" autocomplete="new-password" v-model="newPassword" required />
+          </fieldset>
+          <button type="submit" class="btn btn-primary">S'inscrire</button>
+        </form>
+        <p>Déjà membre ? <a href="#" v-on:click="currentForm++"><strong>Se connecter</strong></a></p>
+      </div>
+      <div class="text-center" v-if="currentForm == 1">
+        <h3>Bienvenue</h3>
+        <form>
+          <fieldset>
+            <input type="text" class="form-control" placeholder="Nom d'utilisateur" autocomplete="username" v-model="username" required />
+            <input type="password" class="form-control" placeholder="Mot de passe" autocomplete="current-password" v-model="password" required />
+          </fieldset>
+          <button type="submit" class="btn btn-primary">Se connecter</button>
+        </form>
+        <p>Pas encore membre ? <a href="#" v-on:click="currentForm--"><strong>S'inscrire</strong></a></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      jokes: [
+        'de la crème',
+        'des internets',
+        'de la DA2I',
+        'de J2EE',
+        'de la crème',
+        'des théories du complot',
+        'des photos de chat',
+        'des mèmes'
+      ],
+      joke: '',
+      currentForm: 0
+    }
+  },
+  mounted: function () {
+    var self = this
+    setInterval(function () {
+      self.$data.joke = self.$data.jokes[Math.floor(Math.random() * self.$data.jokes.length)]
+    }, 3000)
+    this.joke = this.jokes[Math.floor(Math.random() * this.jokes.length)]
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+form fieldset input.form-control {
+    border-radius: 0;
+    margin-top: -1px;
+}
+
+form fieldset input.form-control:first-child {
+    border-radius: 5px 5px 0 0;
+    margin-top: 1em;
+}
+
+form fieldset input.form-control:last-child {
+    border-radius: 0 0 5px 5px;
+    margin-bottom: 1em;
+}
+
+form button {
+    text-transform: uppercase;
+    min-width: 100%;
+    margin-bottom: 1em;
+}
+</style>
