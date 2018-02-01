c7e09755910afe0abe1892bb980302da6863d8d1
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-01 09:57:53 +0100

    Page de recherche d'amis

---
+++ src/components/FriendSearch.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Recherche de « {{ $route.params.text }} »</h1>
+    <friendbutton name="Ami 1"></friendbutton>
+    <friendbutton name="Ami 2"></friendbutton>
+    <friendbutton name="Ami 3"></friendbutton>
+  </div>
+</template>
+
+<script>
+import friendbutton from './friendbutton.vue'
+export default {
+  name: 'FriendSearch',
+  components: {
+    friendbutton
+  }
+}
+</script>
