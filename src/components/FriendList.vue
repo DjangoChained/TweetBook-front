3cb6ef15fa2d2fefcc3cab3645a14bbb1358fe4c
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-01 13:21:44 +0100

    Correction d'un bug visuel par abandon de la recherche de logique dans le CSS

--- src/components/FriendList.vue
+++ src/components/FriendList.vue
@@ -3,9 +3,9 @@
       <h2>Trouver de nouveaux amis</h2>
       <form class="form-inline mb-5">
         <div class="input-group w-100">
-          <input type="text" class="form-control" placeholder="Entrez un nom" v-on:keyup.13="doSearch" v-model="searchText" />
+          <input type="text" class="form-control" placeholder="Entrez un nom" v-on:keyup.13="doSearch" v-model="searchText" style="height: 2.5em" />
           <div class="input-group-append">
-            <button class="btn btn-secondary" type="button" v-on:click="doSearch" v-bind:disabled="$data.searchText.trim().length <= 0">Rechercher</button>
+            <button class="btn btn-secondary" type="button" style="height: 2.5em" v-on:click="doSearch" v-bind:disabled="$data.searchText.trim().length <= 0">Rechercher</button>
           </div>
         </div>
       </form>
