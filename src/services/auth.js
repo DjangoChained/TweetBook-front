8f798f148c69fa2f72480db248b7688e689e584f
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-01 13:20:32 +0100

    Ajout de services d'authentification factices

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,11 @@
 export default {
-  authenticated: true
+  authenticated: false,
+  login: function (username, password) {
+    this.authenticated = true
+    return this.authenticated
+  },
+  logout: function () {
+    this.authenticated = false
+    return true
+  }
 }
