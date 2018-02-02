9b7c414cfa035a458e710be6b750ec5cf722020d
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-02 09:24:19 +0100

    Formulaire d'envoi de publication texte

---
+++ src/components/textpostform.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+      <div class="form-row">
+        <div class="col mb-2 mb-sm-0"><textarea class="form-control" placeholder="Qu'avez-vous en tête ?" v-model="content" v-on:keyup.enter="sendPost" required></textarea></div>
+        <div class="col-sm-2"><button type="button" class="btn btn-primary w-100" v-on:click="sendPost">Publier</button></div>
+      </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'textpostform',
+  data: function () {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    sendPost: function () {
+      // TODO: Envoi d'une publication texte
+      this.$data.content = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+    text-transform: uppercase;
+}
+</style>
