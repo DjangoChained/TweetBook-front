721fb0c30c88464bd7fde8866d5eb34bf85d1c18
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-02 09:25:00 +0100

    Début de la page de flux d'actualités

---
+++ src/components/Feed.vue
@@ -0,0 +1,13 @@
+<template>
+  <postform />
+</template>
+
+<script>
+import postform from '@/components/postform'
+export default {
+  name: 'Feed',
+  components: {
+    postform
+  }
+}
+</script>
