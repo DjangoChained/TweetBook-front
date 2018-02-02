701db46fca2a01112665a54a4ea50446d56ec32c
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-02 09:24:47 +0100

    Composant de saisie de publication

---
+++ src/components/postform.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <nav class="nav nav-pills">
+        <button class="nav-item nav-link btn btn-link" v-bind:class="{'active': currentType === 'text' }" v-on:click="currentType = 'text'">Texte</button>
+        <button class="nav-item nav-link btn btn-link" v-bind:class="{'active': currentType === 'photo' }" v-on:click="currentType = 'photo'">Photo</button>
+        <button class="nav-item nav-link btn btn-link" v-bind:class="{'active': currentType === 'link' }" v-on:click="currentType = 'link'">Lien</button>
+    </nav>
+    <textpostform v-show="currentType === 'text'" />
+    <linkpostform v-show="currentType === 'link'" />
+</div>
+</template>
+
+<script>
+import textpostform from '@/components/textpostform'
+import linkpostform from '@/components/linkpostform'
+export default {
+  name: 'postform',
+  data: function () {
+    return {
+      currentType: 'text'
+    }
+  },
+  components: {
+    textpostform,
+    linkpostform
+  }
+}
+</script>
+
+<style scoped>
+.btn-link {
+    color: #446E9B;
+    text-transform: uppercase;
+}
+</style>
