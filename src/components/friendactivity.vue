9f711ec25379a6a1fa59bf42085bca06c9a66ae5
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-05 17:29:44 +0100

    Ébauche d'activité d'ajout en ami

---
+++ src/components/friendactivity.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="activity">
+        <div class="fa fa-user"></div> <strong>{{ author }}</strong> est devenu ami avec <strong>{{ friend }}</strong> <em>{{ date }}</em>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'friendactivity',
+  props: ['author', 'friend', 'date']
+}
+</script>
+
+<style scoped>
+.activity {
+    margin-left: 1em;
+    margin-bottom: 2em;
+}
+.fa {
+    font-size: 1.7em;
+    margin-right: 0.5em;
+    width: 1.4em;
+    text-align: center;
+    border-radius: 100px;
+    border: 5px solid lightgray;
+    color: #446E9B;
+}
+</style>
