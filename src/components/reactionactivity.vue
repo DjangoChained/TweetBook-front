4920691ce574007fb8f0e4674c70da183c86cf1c
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-07 08:22:36 +0100

    Ajout de l'activité de réaction

---
+++ src/components/reactionactivity.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="activity">
+        <div class="fa fa-user"></div> <strong>{{ author }}</strong> a aimé <a v-bind:href="'#/post/'+postId"><strong>une publication</strong></a> de <strong>{{ postAuthor }}</strong> <em>{{ date }}</em>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'friendactivity',
+  props: ['author', 'postId', 'postAuthor', 'reaction', 'date']
+}
+</script>
+
+<style scoped>
+.activity {
+    margin-left: 1em;
+    margin-bottom: 2em;
+}
+.fa {
+    font-size: 1.7em;
+    margin-right: 0.5em;
+    width: 1.4em;
+    text-align: center;
+    border-radius: 100px;
+    border: 5px solid lightgray;
+    color: #446E9B;
+}
+</style>
