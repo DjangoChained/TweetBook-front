7b0c476af02ea255df813284c55a88fa048b87ab
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-21 10:23:34 +0100

    Abstraction du type de post

---
+++ src/components/post.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <textpost :post="json" v-show="json.type === 'text'" />
+        <imagepost :post="json" v-show="json.type === 'photo'" />
+        <linkpost :post="json" v-show="json.type === 'link'" />
+    </div>
+</template>
+
+<script>
+import textpost from '@/components/textpost'
+import imagepost from '@/components/imagepost'
+import linkpost from '@/components/linkpost'
+export default {
+  name: 'ViewPost',
+  props: ['json'],
+  components: {
+    textpost,
+    imagepost,
+    linkpost
+  }
+}
+</script>
