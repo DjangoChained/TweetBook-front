ef983e9f0e81f8f2f26eabb497e588add14b471f
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-02-21 10:22:58 +0100

    Utilisation de vue-resource

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
 import 'bootstrap'
@@ -8,6 +9,8 @@ import 'bootswatch/dist/spacelab/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.http.options.root = 'http://localhost:8080/TweetLivre'
 
 /* eslint-disable no-new */
 new Vue({
