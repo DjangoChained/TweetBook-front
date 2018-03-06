9a2c9090779b16c99acc62fb98fcf25b4f6ed5de
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-03-06 22:19:00 +0100

    Implémentation du flux d'actualités

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
     <postform />
+    <activity v-for="activity in activities" :key="activity.id" :json="activity" />
   </div>
 </template>
 
 <script>
 import postform from '@/components/postform'
+import activity from '@/components/activity'
+import { getFeed } from '@/services/post'
 export default {
   name: 'Feed',
+  data: function () {
+    return {
+      activities: []
+    }
+  },
   components: {
+    activity,
     postform
+  },
+  mounted: function () {
+    this.$parent.$data.loading = true
+    getFeed().then(res => {
+      this.$data.activities = res.activities
+        .sort((a, b) => ~~(a.date < b.date))
+        .map(act => { act.date = act.date.split('T', 2)[0]; return act })
+      this.$parent.$data.loading = false
+    }, err => {
+      this.$parent.$data.loading = false
+      this.$parent.$data.modal_title = 'Échec du chargement des publications'
+      this.$parent.$data.modal_content = err.message
+      this.$parent.$data.modal = true
+    })
   }
 }
 </script>
