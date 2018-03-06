85e0064a1c5caff105b49299de23d4755144e60b
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-03-06 20:46:41 +0100

    Composant d'activité

---
+++ src/components/activity.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <post :json="json" v-show="['text', 'photo', 'link'].includes(json.type)" />
+        <friendactivity :json="json" v-show="json.type === 'friend'" />
+        <reactionactivity :json="json" v-show="['like', 'dislike', 'reaction'].includes(json.type)" />
+    </div>
+</template>
+
+<script>
+import post from '@/components/post'
+import friendactivity from '@/components/friendactivity'
+import reactionactivity from '@/components/reactionactivity'
+export default {
+  name: 'activity',
+  props: ['json'],
+  components: {
+    post,
+    friendactivity,
+    reactionactivity
+  }
+}
+</script>
