84a7ff2322db4b8a4f5fa4fb9077fadbf09c5ef8
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-03-06 20:46:52 +0100

    Désactivation de la publication d'images

--- src/components/imagepostform.vue
+++ src/components/imagepostform.vue
@@ -2,8 +2,8 @@
   <form>
       <div class="form-row">
         <div class="col-12 col-sm-10 mb-2 order-first">
-          <input type="file" class="form-control form-control-sm mb-1" required />
-          <textarea class="form-control" placeholder="Qu'avez-vous en tête ?" v-model="content" v-on:keyup.enter="sendPost" required></textarea>
+          <input type="file" class="form-control form-control-sm mb-1" required disabled />
+          <textarea class="form-control" placeholder="Qu'avez-vous en tête ?" v-model="content" v-on:keyup.enter="sendPost" required disabled></textarea>
         </div>
         <div class="col-12 col-sm-2 mb-0 mb-sm-2 align-self-center">
           <button type="button" class="btn btn-primary w-100" v-bind:disabled="content.length < 1" v-on:click="sendPost">Publier</button>
