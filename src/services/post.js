cca65c1e6780a2d4db1876b5c8f1dbd5d8558d61
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-03-06 20:47:31 +0100

    API de publications et réactions

---
+++ src/services/post.js
@@ -0,0 +1,8 @@
+import { doGet, doPost, doDelete } from './api'
+export const sendText = content => doPost('wall', {'type': 'text', 'content': content})
+export const sendLink = (content, url, title) =>
+  doPost('wall', {'type': 'link', 'content': content, 'url': url, 'title': title})
+export const getWall = () => doGet('wall')
+export const getFeed = () => doGet('feed')
+export const sendReaction = (id, type) => doPost('reaction', {'reaction': type, 'id_post': id})
+export const removeReaction = id => doDelete('reaction', {'id_post': id})
